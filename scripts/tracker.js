355e948b251052508f33706bb37012ec1dc3b347
jjdale1999 <dalejordanne@gmail.com>
2019-11-30 15:48:14 -0500

    update sql, php and js

---
+++ scripts/tracker.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var loginBtn = document.getElementById("loginBtn");
+
+    loginBtn.onclick = login;
+    
+    function login() {
+        event.preventDefault();
+        httpRequest = new XMLHttpRequest();
+        var url = "tracker.php";
+        httpRequest.onreadystatechange = processLogin;
+        httpRequest.open('POST', url);
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        httpRequest.send('email=' + encodeURIComponent(email) + "&password=" + encodeURIComponent(password));
+    }
+
+    function processLogin() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                var response = httpRequest.responseText;
+                if(response) {
+                    alert("Login successful");
+                    window.location = "Issues.html";
+                }
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
+}
---
+++ sql/tracker.sql
@@ -0,0 +1,40 @@
+/*  CREATION OF DATABASE*/
+DROP DATABASE IF EXISTS users;
+CREATE DATABASE users;
+USE users;
+
+/*  CREATION OF UserInfo Table*/
+DROP TABLE IF EXISTS userInfo;
+CREATE TABLE userInfo(
+id VARCHAR(20)  NOT NULL PRIMARY KEY,
+firstName char(40)  NOT NULL ,
+lastName char(40) NOT NULL,
+pword char(40) NOT NULL,
+email   char(40) NOT NULL,
+date_joined char(10) NOT NULL,
+)
+
+
+
+/*  CREATION OF idInfo Table*/
+
+DROP TABLE IF EXISTS issueInfo;
+CREATE TABLE issueInfo(
+id char(20)  NOT NULL PRIMARY KEY,
+title char(40) DEFAULT NULL,
+descript char(40) DEFAULT NULL,
+typ char(40) DEFAULT NULL,
+pr char(40) DEFAULT NULL,
+stat char(40) DEFAULT NULL,
+assigned_to char(20) DEFAULT NULL,
+created_by char(20) DEFAULT NULL,
+created char(20) DEFAULT NULL,
+updated char(20) DEFAULT NULL,
+)
+
+
+
+
+
+
+insert into xuserInfo values(1,"lol","lol",$hashedPw,'admin@bugme.com',"lol");
