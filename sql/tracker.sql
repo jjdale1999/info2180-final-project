43c3a8514267e1c724df8773352bfc631f8cd02d
jjdale1999 <dalejordanne@gmail.com>
2019-12-01 14:39:01 -0500

    login and home view works

--- sql/tracker.sql
+++ sql/tracker.sql
@@ -21,7 +21,7 @@ DROP TABLE IF EXISTS issueInfo;
 CREATE TABLE issueInfo(
 id char(20)  NOT NULL PRIMARY KEY,
 title char(40) DEFAULT NULL,
-descript char(40) DEFAULT NULL,
+descript char(500) DEFAULT NULL,
 typ char(40) DEFAULT NULL,
 pr char(40) DEFAULT NULL,
 stat char(40) DEFAULT NULL,
@@ -29,10 +29,12 @@ assigned_to char(20) DEFAULT NULL,
 created_by char(20) DEFAULT NULL,
 created char(20) DEFAULT NULL,
 updated char(20) DEFAULT NULL
+
 );
 
+insert into issueInfo values("2","My home page won't load","So I'm doing this webdev project and once I click my homepage, its all code everywhere","Bug","Low","OPEN","admin@bugme.com","corey@fixme.net","12/01/2019","12/01/2019");
+insert into issueInfo values(#)
 
 
-
-
-
+/*HI MY NAME IS COREY HOW CAN I BE OF ASSISTANCE TODAY?*/
+/* more dummy values the ones in the pdf */
