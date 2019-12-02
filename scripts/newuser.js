4dd8ab7ad858b847b4e45371e0f6fa9ed208a8cf
jjdale1999 <dalejordanne@gmail.com>
2019-12-01 21:22:35 -0500

    create user links completed

---
+++ scripts/newuser.js
@@ -0,0 +1,131 @@
+window.onload = function() {
+    var httpRequest;
+    var homeLink = document.getElementById("home");
+    var adduserLink = document.getElementById("adduser");
+    var newissueLink = document.getElementById("newissue");
+    var logoutLink = document.getElementById("logout");
+    var allbtn = document.getElementById("allbtn");
+    var openbtn = document.getElementById("openbtn");
+    var mytickbtn = document.getElementById("myticketsbtn");
+    var titleinfo = document.getElementById('id');
+    
+    
+    
+   
+    homeLink.onclick = loadHome;
+    adduserLink.onclick = loadAddUser;
+    newissueLink.onclick = loadNewIssue;
+    logoutLink.onclick = loadLogout;
+    allbtn.onclick=queryall;
+    openbtn.onclick = queryopen;
+    mytickbtn.onclick=querymyticket;
+    titleinfo.onclick=gettitleinfo;
+    
+    //just testing
+    function gettitleinfo(){
+        event.preventDefault();
+        let page = "home.php";
+        let stateObj = {page: "home"};
+        history.pushState(stateObj, null, "home");
+        requestContent("scripts/"+page+"?id=");
+        document.title = 'BugMe Tracker | Home';
+    }
+    function querymyticket(){
+        event.preventDefault();
+        let page = "home.php";
+        let stateObj = {page: "home"};
+        history.pushState(stateObj, null, "home");
+        requestContent("scripts/"+page+"?home=myticket");
+        document.title = 'BugMe Tracker | Home';
+    }
+
+    function queryall(){
+        event.preventDefault();
+        let page = "home.php";
+        let stateObj = {page: "home"};
+        history.pushState(stateObj, null, "home");
+        requestContent("scripts/"+page+"?home=all");
+        document.title = 'BugMe Tracker | Home';
+    }
+
+    function queryopen() {
+        event.preventDefault();
+        let page = "home.php";
+        let stateObj = {page: "home"};
+        history.pushState(stateObj, null, "home");
+        requestContent("scripts/"+page+"?home=open");
+        document.title = 'BugMe Tracker | Home';
+    }
+
+    function loadHome() {
+        event.preventDefault();
+        let page = "home.php";
+        let stateObj = {page: "home"};
+        history.pushState(stateObj, null, "home");
+        requestContent("scripts/"+page+"?home=home");
+        document.title = 'BugMe Tracker | Home';
+
+    }
+
+    function loadAddUser() {
+        event.preventDefault();
+        let page = "adduser.php";
+        let stateObj = {page: "adduser"};
+        history.pushState(stateObj, null, "adduser");
+        requestContent("scripts/"+page);
+        document.title = 'BugMe Tracker | Add User';
+    }
+
+    function loadNewIssue() {
+        event.preventDefault();
+        let page = "newissue.php";
+        let stateObj = {page: "newissue"};
+        history.pushState(stateObj, null, "newissue");
+        requestContent("scripts/"+page);
+        document.title = 'BugMe Tracker | New Issue';
+    }
+
+    function loadLogout() {
+        event.preventDefault();
+        window.location = "index.html";
+        /*
+        let page = "logout.php";
+        let stateObj = {page: "logout"};
+        history.pushState(stateObj, null, "logout");
+        requestContent("scripts/"+page);
+        document.title = 'BugMe Tracker | Logout';*/
+    }
+
+    function requestContent(filename) {
+        httpRequest = new XMLHttpRequest();
+        var url = filename;
+        httpRequest.onreadystatechange = loadPage;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    }
+
+
+    window.onpopstate = function(event) {
+        let page = history.state.page;
+        let filename = page + ".php";
+    
+        // load the page and put it's contents in the main element.
+        requestContent("scripts/"+filename);
+    
+        // Update the page title in the browser tab
+        document.title = 'BugMe Tracker | ' + page;
+    
+      };
+
+    function loadPage() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                var response = httpRequest.responseText;
+                document.getElementById("container").innerHTML = response;
+                
+            } else {
+                // alert('There was a problem with the request.');
+            }
+        }
+    }
+}
